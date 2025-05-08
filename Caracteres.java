cb1bc40da6f2d2093d8d0a5172bb9c280fe97c52
aliiyjm <20243ds130@utez.edu.mx>
2025-05-08 08:56:41 -0600

    Se imprimieron los valores de las variables

---
+++ Caracteres.java
@@ -0,0 +1,11 @@
+public class Caracteres {
+    public static void main(String [] args){
+        char letra = 'a';
+        char simbolo = '$';
+        char numeroChar = '5';
+
+        System.out.println("letra = " + letra);
+        System.out.println("simbolo = " + simbolo);
+        System.out.println("numeroCha = " + numeroChar);
+    }
+}
